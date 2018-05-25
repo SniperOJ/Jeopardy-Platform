26fcdf5c86df941c0a8af7550282751f6d33da8c
WangYihang <wangyihanger@gmail.com>
2018-05-25 09:40:08 -0400

    [+] Change model of challenge

--- challenge/models.py
+++ challenge/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 
+
 class ChallengeTypes(models.Model):
     name = models.CharField(max_length=8)
     description = models.TextField()
+
 
 # Create your models here.
 class Challenge(models.Model):
     name = models.CharField(max_length=0x20, null=False)
     description = models.TextField()
-    # references
-    # attachments
     visited_times = models.PositiveIntegerField(null=False, default=0)
-    sovled_times = models.PositiveIntegerField(null=False, default=0)
-    # rank
+    solved_times = models.PositiveIntegerField(null=False, default=0)
     challenge_type = models.ForeignKey(ChallengeTypes, on_delete=models.CASCADE)
     points = models.IntegerField(default=1000)
+
+
+class Reference(models.Model):
+    url = models.URLField()
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+
+
+class Attachment(models.Model):
+    url = models.URLField()
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
